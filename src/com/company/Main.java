f35fcf61c7db097c875bfc6b5f9daa144ccc1dc5
shiotsuki ryusei <e195761@ie.u-ryukyu.ac.jp>
2019-12-24 08:42:32 +0900

    レポート４に使用

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,8 @@
 package com.company;
 
-import java.io.IOException;
-
-public class Main {
-
-    public static void main(String[] args) {
-        // write your code here
-        try {
-            String str = null;
-            System.out.println(str.length());
-        }
-        catch (NullPointerException e) {
-            System.out.println("NullPointerExceptionが発生しました");
-            System.out.println(e.getMessage());
-        }
+public class Main{
+    public static void main(String[] args){
+        String str = "百二十三";
+        int i = Integer.parseInt(str);
     }
 }
